b8c2fdb650adbf06a264a8f36362add90d634d64
Media4tech Srl <info@media4tech.com>
2020-02-10 11:00:40 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,3 +4,14 @@
 * When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5
 * (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 */
+var stoca = "";
+for (var i = 0; i < 100; i++) {
+  stoca = "";
+  if (i % 3) {
+    stoca = "Fizz";	
+  }
+  if (i % 5) {
+	stoca+="Buzz";
+  }
+  console.log(stoca);
+}
