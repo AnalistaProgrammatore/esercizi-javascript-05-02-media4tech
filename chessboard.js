52fc40fdb5bd948dbda04c463872fcb8d85bb60c
Media4tech Srl <info@media4tech.com>
2020-02-12 11:21:00 +0100

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -4,10 +4,10 @@
 * The characters should form a chessboard.
 */
 
-var stoca = [""];
+var stoca = "";
 
 for (var i = 0; i < 10; i++) {
-	stoca = [""];
+	stoca = "";
   if (i % 2) {
     for (var x = 0; x < 10; x++) {
 		stoca += " #";
